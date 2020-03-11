237746d8607d0dfc0db93383b6ac4372ae1cba5f
kobi kahila <kobik69@gmail.com>
2020-03-11 20:21:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _11_3_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter 2 numbers (sheerit test))");
+
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (a % b == 0)
+            {
+                Console.WriteLine("no sheerit");
+            }
+            else
+            {
+                Console.WriteLine("sheerit");
+            }
+            Console.ReadKey();
+        }
+    }
+}
